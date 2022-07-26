9e504998346a726a9a38474d535e3b67cfeeefcd
Anuar2 <orazbekov.anvar2@gmail.com>
2022-07-26 12:58:30 +0600

    dfgdfgdf

--- NHW - BerlinClock/main.swift
+++ NHW - BerlinClock/main.swift
@@ -42,7 +42,9 @@ func berlinClockHour(hours: Int) -> String {
     let divider = hours / 5
     let mod = hours % 5
     let count = divider + mod
-    let result = [String](repeating: "R", count: count)
+    var result = [String](repeating: "R", count: count)
+    result.append(" ")
+    result.append(lampRow(totalNumberLamps: 4, numberLampsOn: mod, lampSymbol: "R"))
     return result.joined()
 }
 
@@ -53,17 +55,33 @@ func validateAndGetResult(minutes: Int) -> String {
     let mod = minutes % 5
     
     let yellowResultOfFive = [String](repeating: "Y", count: divider)
-    let realResultOfFive: [String] = yellowResultOfFive.enumerated().compactMap { (index, result) in
+    var realResultOfFive: [String] = yellowResultOfFive.enumerated().compactMap { (index, result) in
         let redPartIndexes = [3, 6, 9]
         return redPartIndexes.contains(index + 1) ? "R" : result
     }
     
+    realResultOfFive.append(" ")
+    realResultOfFive.append(lampRow(totalNumberLamps: 4, numberLampsOn: mod, lampSymbol: "Y"))
     let resultOfOne = [String](repeating: "Y", count: mod)
     let result = realResultOfFive + resultOfOne
     
     return result.joined()
 }
 
+
+func lampRow(totalNumberLamps: Int , numberLampsOn: Int , lampSymbol: String ) -> String {
+    var sb: String = ""
+    let intStream = 1...totalNumberLamps
+    intStream.forEach { i in
+        sb.append( i <= numberLampsOn ? lampSymbol : "O" )
+    }
+    return sb
+    
+}
+
+func minuteRow(index: Int) -> String {
+    return index % 3 == 0 ? "R" : "Y";
+}
 
 print(berlinClockHour(hours: 20))
 print(validateAndGetResult(minutes: 55))
